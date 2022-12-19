f138c0f7c4eec15182ec1031f66103f003692c5a
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-12-19 19:52:57 +0100

    last commit

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ import { Atlas } from './atlas.js';
 import { RESTDataSource } from './rest-data-source.js';
 
 /** The data source for our Atlas */
-//const dataSource = new RESTDataSource("https://caha1906-lab1-backend-dt190g.azurewebsites.net/");
-const dataSource = new RESTDataSource("http://localhost:3000");
+const dataSource = new RESTDataSource("https://caha1906-lab2-backend-dt190g.azurewebsites.net/");
+//const dataSource = new RESTDataSource("http://localhost:3000");
 
 /** The Atlas instance */
 const atlas = new Atlas(dataSource);
