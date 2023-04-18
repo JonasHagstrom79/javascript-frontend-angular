d5515d7c39ee540178ed0b4b807a37912c307314
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2023-04-18 08:05:26 +0200

    installed angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'caha1906-lab-frontend-dt190g';
+}
