2bcbb4a2aebbf3105fec143e97bb1accb5ee8858
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2023-05-21 02:08:44 +0200

    småfix

---
+++ src/app/my-course.ts
@@ -0,0 +1,12 @@
+export interface MyCourse {
+    _id: string;
+    courseCode: string;
+    subjectCode: string;
+    level: string;
+    progression: string;
+    name: string;
+    points: number;
+    institutionCode: string;
+    subject: string;
+    grade: string;
+}
