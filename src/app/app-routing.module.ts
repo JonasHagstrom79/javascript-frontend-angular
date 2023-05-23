47625772f19e0958b9b17cade2497b1525ecbf0c
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2023-05-23 10:54:07 +0200

    Routning works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { HomeComponent } from './app.component'; //TODO:remove?
 import { CoursesComponent } from './courses/courses.component';
 import { MyCoursesComponent } from './my-courses/my-courses.component';
 import { MyCoursesRoutingModule } from './my-courses/my-courses-routing.module';//TODO:remove?
 
 
 const routes: Routes = [
+  { path: '', component: CoursesComponent},
   { path: 'courses', component: CoursesComponent },
   { path: 'mycourses', component: MyCoursesComponent}
 ];
